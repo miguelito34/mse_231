9db5542c7a138fbc2efecbedc592c75be4c58056
afdocherty <afdoch@stanford.edu>
2019-11-14 21:14:40 -0800

    Able to pull data via IDs

--- scripts/open311_pull_all_data.py
+++ scripts/open311_pull_all_data.py
@@ -8,13 +8,9 @@ import pandas as pd
 import datetime as dt
 import csv
 
-# Constants
-OPEN311_RANGE = 90 # Open311 can only take 90 requests at a time
-#DATA_LIMIT_ADJUSTMENT = dt.timedelta(minutes = 10)
-
 ## Domains for available Open311 GeoReport v2 city APIs
 DOMAINS = {
-    #"Bloomington_IN": "bloomington.in.gov/crm",
+    "Bloomington_IN": "bloomington.in.gov/crm",
     "Boston_MA": "311.boston.gov",
     "Brookline, MA": "spot.brooklinema.gov",
     "Chicago, IL": "311api.cityofchicago.org",
@@ -22,18 +18,35 @@ DOMAINS = {
     "San Francisco_CA": "mobile311.sfgov.org"
 }
 
-DATA_LIMITS = {
-    #"Bloomington_IN": 1000,
-    "Boston_MA": 50,
-    "Brookline, MA": 100,
-    "Chicago, IL": 50,
-    "Peoria_IL": 1000,
-    "San Francisco_CA": 50
+SERVICE_REQUEST_ID_END = {
+    "Bloomington_IN": "170962",
+    "Boston_MA": "101003103001",
+    "Brookline, MA": "22925",
+    "Chicago, IL": "02950001",
+    "Peoria_IL": "27365",
+    "San Francisco_CA": "11687000"
 }
 
-def get_requests_response_root(domain, start_datetime_string, end_datetime_string, status):
-    """Function to get the requests for a given domain, start/end datetime, and status"""
-    response = requests.get(f"http://{domain}/open311/v2/requests.xml?start_date={start_datetime_string}&end_date={end_datetime_string}&status={status}")
+# Cities that support comma-delimeted IDs
+COMMA = {
+    "Brookline, MA": "100"
+}
+
+COMMA_AND_PAGE_SIZE = {
+    "Boston_MA": "250",
+    "Chicago, IL": "500",
+    "San Francisco_CA": "500"
+}
+
+def get_requests_response_root(domain, service_request_id):
+    """Function to get a request by service_request_id"""
+    response = requests.get(f"http://{domain}/open311/v2/requests.xml?service_request_id={service_request_id}")
+    root = ElementTree.fromstring(response.content)
+    return(root)
+
+def get_requests_response_root_page_size(domain, service_request_id, page_size):
+    """Function to get a request by service_request_id"""
+    response = requests.get(f"http://{domain}/open311/v2/requests.xml?page_size={page_size},service_request_id={service_request_id}")
     root = ElementTree.fromstring(response.content)
     return(root)
 
@@ -41,7 +54,7 @@ def datetime_to_string(datetime_obj):
     """Function to convert a datetime into a request-compatible string"""
     return(f"{datetime_obj.date()}T{datetime_obj.time()}Z")
 
-def get_requests_data(vars_list, status, start_datetime, num_90day_iters=1, print_header=True):    
+def get_requests_data(vars_list, print_header=True):    
     # Get current datetime and datetime for start of today
     now = dt.datetime.now()
     today = now - dt.timedelta(hours = now.hour, minutes = now.minute, seconds = now.second, microseconds = now.microsecond)
@@ -52,70 +65,128 @@ def get_requests_data(vars_list, status, start_datetime, num_90day_iters=1, prin
         vars_string = "\t".join(vars_dummy_dict.keys())
         print(f"city\t{vars_string}")
 
+    min_data_points_per_city = 2
     # Iterate over cities
     for city, domain_value in DOMAINS.items():
-        # Reset end datetime to today
-        end_datetime = today - dt.timedelta(days = 1)
-        # Loop until we have covered the entire requested datetime range
-        while (end_datetime.date() > start_datetime.date()):
+        # Start with the id specified for each city
+        curr_service_request_id = SERVICE_REQUEST_ID_END[city]
+        # Keep track of how many data points we have printed for this city
+        total_data_count = 0
+        while (total_data_count < min_data_points_per_city):
+            # If the city supports comma-delimeted IDs and page_size, ask for multiple with max page_size
+            # Elif the city supports comma-delimeted IDs, ask for multiple. 
+            # Else, request IDs one-by-one
+            if (city in COMMA_AND_PAGE_SIZE):
+                curr_num = int(curr_service_request_id)
+                num_minus_page_size = curr_num - int(COMMA_AND_PAGE_SIZE[city])
+                service_request_id_list = [str(i) for i in range(num_minus_page_size + 1, curr_num + 1)]
+                service_request_id_list_string = ",".join(service_request_id_list)
 
-            # Make the GET request for the data and receive the root of the XML-parsed ElementTree
-            print(f"Making request: city={city}, start={start_datetime}, end={end_datetime}, status={status}") # DEBUG
-            root = get_requests_response_root(domain_value, 
-                                              datetime_to_string(start_datetime), 
-                                              datetime_to_string(end_datetime),
-                                              status)
-            # Counter to keep track of how many data points we have already printed
-            data_count = 0
-            # Chicago fix
-            requested_datetime_list = []
-            # Iterate over the 311 requests we received
-            for request in root.iter('request'):
-                request_data = {}
+                # Make the GET request for the data and receive the root of the XML-parsed ElementTree
+                #print(f"Making request: city={city}, starting with curr_service_request_id={curr_service_request_id}, page_size={COMMA_AND_PAGE_SIZE[city]}") # DEBUG
+                root = get_requests_response_root_page_size(domain_value, 
+                                                  service_request_id_list_string,
+                                                  COMMA_AND_PAGE_SIZE[city])
+                # Try to get a 311 request from the id
+                request_test = root.find("request")
+                if request_test is not None:
+                    for request in root.iter("request"):
+                        # Extract request data and store in dict
+                        request_data = {}
+                        for var in vars_list:
+                            try:
+                                data = request.find(var).text
+                                data.replace("\t","")
+                                request_data[var] = data
+                            except AttributeError:
+                                request_data[var] = ""
+                            if (request_data[var] is None):
+                                request_data[var] = ""
 
-                for var in vars_list:
-                    try:
-                        data = request.find(var).text
-                        data.replace("\t","")
-                        request_data[var] = data
-                    except AttributeError:
-                        request_data[var] = ""
-                    if (request_data[var] is None):
-                        request_data[var] = ""
+                        if (request_data["service_name"] != "" and request_data["lat"] != "" and request_data["long"] != ""):                    
+                            # Join the request data into a tab-separated string 
+                            request_data_values_string = "\t".join(list(request_data.values()))
 
-                # Chicago fix
-                if (city == "Chicago, IL"):
-                    requested_datetime_list.append(request_data["requested_datetime"])
+                            # Print with the associated city
+                            print(f"{city}\t{request_data_values_string}")
 
-                # Used to ensure that we have a valid end datetime to reset to
-                if ("updated_datetime" in request_data):
-                    end_datetime = dt.datetime.strptime(request_data["updated_datetime"][:19], "%Y-%m-%dT%H:%M:%S")
-                    # print(f"current_end_datetime={current_end_datetime}") # DEBUG
+                            # Increment data_count for this data point
+                            total_data_count += 1
+                # Decrement id for requesting prev id 
+                curr_service_request_id = str(num_minus_page_size)
+            elif(city in COMMA):
+                curr_num = int(curr_service_request_id)
+                num_minus_page_size = curr_num - int(COMMA[city])
+                service_request_id_list = [str(i) for i in range(num_minus_page_size + 1, curr_num + 1)]
+                service_request_id_list_string = ",".join(service_request_id_list)
 
-                # Join the output variables into a tab-separated string 
-                request_data_values_string = "\t".join(list(request_data.values()))
-                # Print with the associated city
-                print(f"{city}\t{request_data_values_string}")
+                # Make the GET request for the data and receive the root of the XML-parsed ElementTree
+                #print(f"Making request: city={city}, starting with curr_service_request_id={curr_service_request_id}") # DEBUG
+                root = get_requests_response_root(domain_value, 
+                                                  service_request_id_list_string)
+                # Try to get a 311 request from the id
+                request_test = root.find("request")
+                if request_test is not None:
+                    for request in root.iter("request"):
+                        # Extract request data and store in dict
+                        request_data = {}
+                        for var in vars_list:
+                            try:
+                                data = request.find(var).text
+                                data.replace("\t","")
+                                request_data[var] = data
+                            except AttributeError:
+                                request_data[var] = ""
+                            if (request_data[var] is None):
+                                request_data[var] = ""
 
-                # Increment the data count for this data point
-                data_count += 1
+                        if (request_data["service_name"] != "" and request_data["lat"] != "" and request_data["long"] != ""):                    
+                            # Join the request data into a tab-separated string 
+                            request_data_values_string = "\t".join(list(request_data.values()))
 
-                # If we hit or exceeded or data points limit for that city, make a new data request the latest datetime.
-                if (data_count >= DATA_LIMITS[city]):
-                    print(f"Hit limt. data_count={data_count}, data_limit={DATA_LIMITS[city]}") # DEBUG
-                    break
+                            # Print with the associated city
+                            print(f"{city}\t{request_data_values_string}")
 
-            # For Chicago, store and sort the series of requests by requested_datetime, 
-            # using the min datetime as the new end_datetime 
-            if (city == "Chicago, IL"):
-                requested_datetime_list.sort()
-                end_datetime = dt.datetime.strptime(requested_datetime_list[0][:19], "%Y-%m-%dT%H:%M:%S")
+                            # Increment data_count for this data point
+                            total_data_count += 1
+                # Decrement id for requesting prev id 
+                curr_service_request_id = str(num_minus_page_size)
+            else:
+                # Make the GET request for the data and receive the root of the XML-parsed ElementTree
+                #print(f"Making request: city={city}, curr_service_request_id={curr_service_request_id}") # DEBUG
+                root = get_requests_response_root(domain_value, 
+                                                  curr_service_request_id)
+
+                # Try to get a 311 request from the id
+                request = root.find("request")
+                if request is not None:
+                    # Extract request data and store in dict
+                    request_data = {}
+                    for var in vars_list:
+                        try:
+                            data = request.find(var).text
+                            data.replace("\t","")
+                            request_data[var] = data
+                        except AttributeError:
+                            request_data[var] = ""
+                        if (request_data[var] is None):
+                            request_data[var] = ""
+
+                    # Join the request data into a tab-separated string 
+                    request_data_values_string = "\t".join(list(request_data.values()))
+
+                    # Print with the associated city
+                    print(f"{city}\t{request_data_values_string}")
+
+                    # Increment data_count for this data point
+                    total_data_count += 1
+
+                # Decrement id for requesting prev id 
+                curr_service_request_id = str(int(curr_service_request_id) - 1)
                     
 def main():
-    vars_list = ["service_request_id","status","status_notes","service_name","service_code","requested_datetime","updated_datetime","address","lat","long"]
-    start_datetime = dt.datetime(2019, 11, 1, 0, 0, 0)
-    get_requests_data(vars_list, "closed", start_datetime)
-    #get_requests_data(vars_list, "closed", start_datetime, print_header=False)
+    vars_list = ["service_request_id","status","service_name","service_code","requested_datetime","updated_datetime","address","lat","long"]
+    get_requests_data(vars_list)
 
 if __name__== "__main__":
     main()
