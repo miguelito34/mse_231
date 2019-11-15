6b3a9762edcb88777d3709130858ce71f28c7cfa
afdocherty <afdoch@stanford.edu>
2019-11-14 19:14:27 -0800

    Added open311_get_service_list.py and service_names.csv. Updates to open311_pull_all_data.py, not ready for production yet.

--- scripts/open311_get_service_list.py
+++ scripts/open311_get_service_list.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python
+# coding: utf-8
+
 import xml.etree.ElementTree as ElementTree
 import requests
 import pandas as pd
 
-# GeoReport v2 API endpoinits
-response_SF = requests.get('http://mobile311.sfgov.org/open311/v2/services.xml')
-response_Chicago = requests.get('http://311api.cityofchicago.org/open311/v2/services.xml')
-response_Boston = requests.get('https://311.boston.gov/open311/v2/services.xml')
+DOMAINS = {
+    "Bloomington_IN": "bloomington.in.gov/crm",
+    "Boston_MA": "311.boston.gov",
+    "Brookline, MA": "spot.brooklinema.gov",
+    "Chicago, IL": "311api.cityofchicago.org",
+    "Peoria_IL": "ureport.peoriagov.org/crm",
+    "San Francisco_CA": "mobile311.sfgov.org"
+}
 
-# XML roots
-root_SF = ElementTree.fromstring(response_SF.content)
-root_Chicago = ElementTree.fromstring(response_Chicago.content)
-root_Boston = ElementTree.fromstring(response_Boston.content)
+URL_START = "http://"
+URL_END = "/open311/v2/services.xml"
 
-# Collect service names in Pandas DataFrames
-service_names_SF = []
-for service_name in root_SF.iter('service_name'):
-    service_names_SF.append(service_name.text)
-df_service_names_SF = pd.DataFrame(service_names_SF, columns=["SF"])
+def main():
+    dummy_list = []
+    df_service_names_all = pd.DataFrame(dummy_list, columns=["dummy"])
 
-service_names_Chicago = []
-for service_name in root_Chicago.iter('service_name'):
-    service_names_Chicago.append(service_name.text)
-df_service_names_Chicago = pd.DataFrame(service_names_Chicago, columns=["Chicago"])
+    for key in DOMAINS:
+        response = requests.get(URL_START + DOMAINS[key] + URL_END)
+        root = ElementTree.fromstring(response.content)
+        service_names = []
+        for service_name in root.iter("service_name"):
+            service_names.append(service_name.text)
+        df_service_names = pd.DataFrame(service_names, columns=[key])
+        df_service_names_all = pd.concat([df_service_names_all, df_service_names], axis=1)
 
-service_names_Boston = []
-for service_name in root_Boston.iter('service_name'):
-    service_names_Boston.append(service_name.text)
-df_service_names_Boston = pd.DataFrame(service_names_Boston, columns=["Boston"])
+    df_service_names_all = df_service_names_all.drop(['dummy'], axis=1)
+    df_service_names_all.to_csv('service_names.csv', index=False)
 
-# Concatenate the DataFrames
-df_service_names = pd.concat([df_service_names_SF, df_service_names_Chicago], axis=1) 
-df_service_names = pd.concat([df_service_names, df_service_names_Boston], axis=1) 
-
-# Save to .csv
-df_service_names.to_csv('service_names.csv', index=False)
-
-
-
+if __name__== "__main__":
+    main()
