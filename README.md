5a4717b953898f6c1f37ff11086330d9a6baa5d7
michaelspencer <m1373spencer@gmail.com>
2019-11-18 02:00:04 -0800

    re-wrote 311 and census joining scripts, has inefficient code, but works for now

--- README.md
+++ README.md
@@ -88,14 +88,14 @@ python3 scripts/311/open311_get_service_list.py
 
 Adjusts to this step will be made as the quantity of calls available/needed changes. In the future, this step will likely be piped into the following step so as to output a single file that includes the calls as well as their census tracts. To replicate, run the script as below:
 ```
-python3 scripts/311/open311_pull_all_data.py > data_raw/311/sample_311_data.tsv
+python3 scripts/311/open311_pull_all_data.py > data_raw/311/unjoined/sample_311_data.tsv
 ```
 
 4. Locate each call
 
 In order to perform any significant analysis, we must join this call data with census data. We'll be doing this at the level of census tracts. This script will pair each call with it's relevant geoid, allowing us to join the call with census data in later steps. In this step, we discard any calls for which the location cannot be determined by lat/long.
 ```
-python3 scripts/311/get_census_tract_batch.py > data_raw/311/sample_311_data_geoid.tsv
+python3 scripts/311/get_census_tract_batch.py
 ```
 
 ### Approach and Strategy (To Come)
--- scripts/311/get_census_tract_batch.py
+++ scripts/311/get_census_tract_batch.py
@@ -12,13 +12,24 @@ import rtree
 PROJECT_PATH = os.getcwd()
 
 # Loads relevant census and 311 call data
-census_tracts = gpd.read_file(r'data/census/census_data_shapefiles.shp')[['geoid', 'geometry']]
-calls = pd.read_csv(r'data_raw/311/sample_311_data.tsv', sep = "\t", index_col = 0)
+
+
+#census_tracts = gpd.read_file(r'data/census/census_data_shapefiles.shp')[['geoid', 'geometry']] # Use this to drop columns
+census_tracts = gpd.read_file(r'data/census/census_data_shapefiles.shp') # Use this to keep all columns, i.e. if doing things entirely in memory
+
+
+calls = pd.read_csv(r'data_raw/311/unjoined/sample_311_data_300k_BOS_CHI_SFO.tsv', sep = "\t", index_col = 0)
 calls = calls[calls.lat.notnull() & calls.long.notnull()]
 
 # Creates a spatial dataframe of the calls and their lat/long before joining it with the census data
 gdf = gpd.GeoDataFrame(calls, crs = {'init' :'epsg:4269'}, geometry = [Point(xy) for xy in zip(calls.long, calls.lat)])
+gdf = gdf[gdf.geometry.type == "Point"]
+gdf = gdf[0:100]
+located_311_data = sjoin(gdf, census_tracts, how = 'left').drop(['index_right', 'geometry'], axis = 1)
 
-located_311_data = sjoin(gdf, census_tracts, how='left').drop(['index_right', 'geometry'], axis = 1)
+# I have positively no idea why, but running this again seems to be the only way to make this work, major debugging needed, might try in R.
+gdf = gpd.GeoDataFrame(calls, crs = {'init' :'epsg:4269'}, geometry = [Point(xy) for xy in zip(calls.long, calls.lat)])
+gdf = gdf[gdf.geometry.type == "Point"]
+located_311_data = sjoin(gdf, census_tracts, how = 'left').drop(['index_right', 'geometry'], axis = 1)
 
-located_311_data.to_csv((PROJECT_PATH + '/data_raw/311/sample_311_data_geoid.tsv'), sep = "\t", index=False)
+located_311_data.to_csv((PROJECT_PATH + '/data_raw/311/joined/sample_311_data_300k_BOS_CHI_SFO_census.tsv'), sep = "\t", index = False)
